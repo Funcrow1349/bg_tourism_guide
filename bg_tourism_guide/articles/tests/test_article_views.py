8a963fcd2a4b7434e466325bfa8dc8f603e4e653
Ivaylo Ivanov <funcrow1349@gmail.com>
2023-08-29 13:19:51 +0300

    Initial commit

--- bg_tourism_guide/articles/tests/test_article_views.py
+++ bg_tourism_guide/articles/tests/test_article_views.py
@@ -1,32 +1,57 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase, Client
+from django.urls import reverse
 
 from bg_tourism_guide.articles.models import Article
 
 CustomUser = get_user_model()
 
+VALID_ARTICLE_DATA = {
+    'title': 'Some valid title',
+    'body': 'Some valid body',
+    'article_image': 'https://th.bing.com/th/id/OIP.fSo5HMXhsaMA3S8IfAO_CAHaE7?pid=ImgDet&rs=1',
+}
 
-class ArticleViewTests(TestCase):
-    VALID_ARTICLE_DATA = {
-        'title': 'Some valid title',
-        'body': 'Some valid body',
-        'article_image': 'https://th.bing.com/th/id/OIP.fSo5HMXhsaMA3S8IfAO_CAHaE7?pid=ImgDet&rs=1',
+VALID_AUTHOR_DATA = {
+    'first_name': 'Tester',
+    'last_name': 'Testing',
+    'email': 'some_tester@gmail.com',
+    'username': 'Some tester'
+}
+
+
+def _create_article(**kwargs):
+    author = CustomUser.objects.create(**VALID_AUTHOR_DATA)
+    article_data = {
+        **kwargs,
+        'author': author,
     }
 
-    VALID_AUTHOR_DATA = {
-        'first_name': 'Tester',
-        'last_name': 'Testing',
-        'email': 'some_tester@gmail.com',
-        'username': 'Some tester'
-    }
+    return Article(**article_data)
 
-    def setUp(self):
-        self.test_client = Client()
+
+class ArticleDetailsViewTests(TestCase):
 
     def test_article_detail_view_get_request_returns_correct_response(self):
-        # self.author = CustomUser.objects.create(**self.VALID_AUTHOR_DATA)
-        # self.article = Article.objects.create(**self.VALID_ARTICLE_DATA)
-        response = self.test_client.get('article/')
+        article = _create_article(**VALID_ARTICLE_DATA)
+        article.save()
+
+        response = self.client.get(
+            reverse('article details')
+        )
 
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response, 'articles/article_details.html')
+
+
+class ArticleCreateViewTests(TestCase):
+    pass
+
+
+class ArticleEditViewTests(TestCase):
+    pass
+
+
+class ArticleDeleteViewTests(TestCase):
+    pass
+

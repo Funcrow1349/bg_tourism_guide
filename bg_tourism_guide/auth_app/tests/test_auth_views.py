279ff380b21837af52ddf65ebff287fcf50a7b19
Ivaylo Ivanov <funcrow1349@gmail.com>
2023-08-30 18:31:22 +0300

    Initial commit

--- bg_tourism_guide/auth_app/tests/test_auth_views.py
+++ bg_tourism_guide/auth_app/tests/test_auth_views.py
@@ -1,5 +1,25 @@
 from django.test import TestCase
 
 
-class AuthViewTests(TestCase):
+class ProfileDetailsViewTests(TestCase):
+    pass
+
+
+class ProfileCreateViewTests(TestCase):
+    pass
+
+
+class ProfileEditViewTests(TestCase):
+    pass
+
+
+class ProfileDeleteViewTests(TestCase):
+    pass
+
+
+class LoginViewTests(TestCase):
+    pass
+
+
+class LogoutViewTests(TestCase):
     pass
